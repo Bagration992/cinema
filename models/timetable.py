28815c7bc63c8d9da9ffbabcfadf144ff00174c7
Bagration992 <n_majstorovic@protonmail.com>
2019-07-19 16:08:07 +0200

    [ADD] Computed fields added (part 6 of Part2)

--- models/timetable.py
+++ models/timetable.py
@@ -15,3 +15,18 @@ class timetable(models.Model):
 
     room = fields.Many2one(comodel_name="cinema.room", string="Room", required=False)
 
+    total_seats = fields.Integer(compute="_get_total_seats", store=True, string="Total seats")
+
+    sold_seats = fields.Integer(string="Number of sold seats", store=True, default=1)
+
+    remaining_seats = fields.Integer(compute="_get_remaining_seats", store=True, string="Number of seats remaining")
+
+    @api.depends('room')
+    def _get_total_seats(self):
+        for row in self:
+            row.total_seats = row.room.capacity
+
+    @api.depends('sold_seats', 'total_seats')
+    def _get_remaining_seats(self):
+        for row in self:
+            row.remaining_seats = row.total_seats - row.sold_seats
